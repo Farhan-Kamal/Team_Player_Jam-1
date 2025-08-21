89596ca8f1294e105046106ad35f39d8f8eee303
Far <farhankamal0505@gmail.com>
2025-08-21 13:43:01 -0400

    Working on Functionality
    Yes.

--- Assets/CombatManger.cs
+++ Assets/CombatManger.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CombatManger : MonoBehaviour
 {
@@ -16,15 +18,24 @@ public class CombatManger : MonoBehaviour
     public int P2Health = 5;
     public int P2Shield = 3;
     public int Et = 1;
+    public Slider PHealth;
+    public Slider PShield;
+    public int EHeal = 5;
+    public int EShiel = 5;
+    public Slider EHealth;
+    public Slider EShield;
     // Start is called before the first frame update
     void Start()
     {
-        Et = 5;
+        // Et = Random.Range(0, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PHealth.value = P1Health;
+        PShield.value = P1Shield;
+        EHealth.value = EHeal;
+        EShield.value = EShiel;
     }
 }
