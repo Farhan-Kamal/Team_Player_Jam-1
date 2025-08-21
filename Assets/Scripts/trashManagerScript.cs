76115f65a35aef9c15a51d7834f7ea1eb303e0cd
afrid-zaman <zukdragonic@GMAIL.COM>
2025-08-21 15:29:40 -0400

    Seller Script

--- Assets/Scripts/trashManagerScript.cs
+++ Assets/Scripts/trashManagerScript.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class trashManagerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static trashManagerScript instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public SO_Trash[] trashArray;
 }
