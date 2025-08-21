dfe21d3661aaf3aea0e8d3451cd3fa42646b5d2d
Far <farhankamal0505@gmail.com>
2025-08-21 11:58:44 -0400

    a

---
+++ Assets/GameManger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Player1Level = 1;
+    public int Player2Level = 1;
+    public int Cash = 3;
+    public int RatD = 1;
+    public int P1Health = 5;
+    public int P1Shield = 3;
+    public int P2Health = 5;
+    public int P2Shield = 3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Tele.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tele : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("I have been Touched.");
+        if (collision != null)
+        {
+            Debug.Log(collision.gameObject.CompareTag("p1"));
+            if (collision.gameObject.CompareTag("p1")) {
+                SceneManager.LoadScene("Combat");
+            }
+        }
+    }
+}
